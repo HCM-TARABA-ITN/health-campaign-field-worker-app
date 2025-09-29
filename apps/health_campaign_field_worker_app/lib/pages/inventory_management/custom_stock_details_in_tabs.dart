bb1f569c01ac0df0e6e4b4675bad63dd61d2a271
Harsh Khatri <harsh.khatri@beehyv.com>
2025-09-29 19:30:21 +0530

    Added bednet in user auth previously was 0

--- apps/health_campaign_field_worker_app/lib/pages/inventory_management/custom_stock_details_in_tabs.dart
+++ apps/health_campaign_field_worker_app/lib/pages/inventory_management/custom_stock_details_in_tabs.dart
@@ -471,7 +471,7 @@ class _DynamicTabsPageState extends LocalizedState<DynamicTabsPage>
                     Row(
                       children: [
                         const Expanded(child: Text('Resource')),
-                        Expanded(child: Text(productName)),
+                        Expanded(child: Text(products.first.variation ?? '')),
                       ],
                     ),
                     const SizedBox(height: 8),
@@ -980,7 +980,7 @@ class _DynamicTabsPageState extends LocalizedState<DynamicTabsPage>
 
       context.read<AuthBloc>().add(
             AuthAddProductCountsEvent(
-              bednetCount: 0,
+              bednetCount: bednetCount,
               spaq1Count: spaq1Count,
               spaq2Count: spaq2Count,
               blueVasCount: 0,
@@ -1004,6 +1004,10 @@ class _DynamicTabsPageState extends LocalizedState<DynamicTabsPage>
 
     final selectedProducts =
         products.map((variant) => variant.sku).whereType<String>().toList();
+    final selectedProductsToShow = products
+        .map((variant) => variant.variation)
+        .whereType<String>()
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -1011,7 +1015,7 @@ class _DynamicTabsPageState extends LocalizedState<DynamicTabsPage>
         bottom: TabBar(
           controller: _tabController,
           isScrollable: true,
-          tabs: selectedProducts
+          tabs: selectedProductsToShow
               .map((product) => Tab(text: product.toUpperCase()))
               .toList(),
         ),
